5324d3414072efb17dd9a635e7164c5c9bb6feb8
ricardovobarros <ricardovobarros@live.com>
2022-05-08 20:56:55 +0200

    balanco e dr done

--- economista.py
+++ economista.py
@@ -4,9 +4,11 @@ from config import *
 class Economista:
     def __init__(self, data="", simples=False):
         # attributes
+        self.repeted_ids = []
         self.data = data
         self.balanco = pd.DataFrame()
         self.dr = pd.DataFrame()
+        self.dr_unique = pd.DataFrame()
         self.indices = pd.DataFrame()
         self.id_unique = []
         self.id_nonunique = []
@@ -16,8 +18,9 @@ class Economista:
             self.irpj = 0  # TODO: definir valor para empresa simples
 
         # call methods
-        self.compute_balanco()
         self.compute_dr()
+        self.compute_dr_unique()
+        self.compute_balanco()
         self.compute_indices()
 
     def compute_balanco(self):
@@ -36,8 +39,8 @@ class Economista:
         # list the entries that have unique code
         id_list_unique = ["LS", "C", "TR", "IM", "SM", "CT", "CN", "IC", "PI",
                           "CO", "IS", "SN", "CV", "CI", "PL", "JMRC", "RAF", "ORF",
-                          "DF_01", "GG_21", "GG_22", "DF_02", "PCNPJ", " IN", "GG_19",
-                          "GG_20", "MK_01", "PL", "DL", "VI", "AS", "REB", "RET", "RES",
+                          "DF_01", "GG_21", "GG_22", "DF_02", "PCNPJ", "GG_19",
+                          "GG_20", "MK_01", "DL", "VI", "AS", "REB", "RET", "RES",
                           "DL", "OEO"]
 
         # list entries that have no unique code
@@ -67,100 +70,99 @@ class Economista:
 
         # deducao sobre receitas
         id_list_dsr = ["IC", "PI", "CO", "IS", "SN"]
-        df = df_unique[df_unique["id"].isin(id_list_dsr)]
-        s = df.sum(axis=0).squeeze()
+        df1 = df_unique[df_unique["id"].isin(id_list_dsr)]
+        s = df1.sum(axis=0).squeeze()
         s.loc["descricao"] = "Deducao Sobre Receita"
         s.loc["id"] = "DR_DSR"
         s = pd.DataFrame(data=[s.values], columns=s.index)
-        self.dr = pd.concat([self.dr, df, s])
+        self.dr = pd.concat([self.dr, df1, s])
 
         # compute receita liquida
         df_values = self.dr[self.dr["id"].isin(["DR_RB", "DR_DSR"])].iloc[:, 2:14]
-        df = df_values.iloc[0, :] - df_values.iloc[1, :]
-        df.loc["id"] = "DR_RL"
-        df.loc["descricao"] = "Receita Liquida"
-        df = pd.DataFrame(data=[df.values], columns=df.index)
-        self.dr = pd.concat([self.dr, df])
+        df2 = df_values.iloc[0, :] - df_values.iloc[1, :]
+        df2.loc["id"] = "DR_RL"
+        df2.loc["descricao"] = "Receita Liquida"
+        df2 = pd.DataFrame(data=[df2.values], columns=df2.index)
+        self.dr = pd.concat([self.dr, df2])
 
         # compute custo variavel
         id_list_cv = ["CV", "CI"]
-        df = df_unique[df_unique["id"].isin(id_list_cv)]
-        s = df.sum(axis=0).squeeze()
+        df3 = df_unique[df_unique["id"].isin(id_list_cv)]
+        s = df3.sum(axis=0).squeeze()
         s.loc["descricao"] = "Custo Variavel"
         s.loc["id"] = "DR_CV"
         s = pd.DataFrame(data=[s.values], columns=s.index)
-        self.dr = pd.concat([self.dr, df, s])
+        self.dr = pd.concat([self.dr, df3, s])
 
         # compute margem de contribuicao
         df_values = self.dr[self.dr["id"].isin(["DR_RL", "DR_CV"])].iloc[:, 2:14]
-        df = df_values.iloc[0, :] - df_values.iloc[1, :]
-        df.loc["id"] = "DR_MC"
-        df.loc["descricao"] = "Margen de Contribuicao"
-        df = pd.DataFrame(data=[df.values], columns=df.index)
-        self.dr = pd.concat([self.dr, df])
+        df4 = df_values.iloc[0, :] - df_values.iloc[1, :]
+        df4.loc["id"] = "DR_MC"
+        df4.loc["descricao"] = "Margen de Contribuicao"
+        df4 = pd.DataFrame(data=[df4.values], columns=df4.index)
+        self.dr = pd.concat([self.dr, df4])
 
         # Despesa fixa
         id_list_df_var = ["PE", "GG", "MK", "DF"]
         id_list_df = ["PL"]
-        df = df_nonunique[df_nonunique["id"].str.contains('|'.join(id_list_df_var))]
-        df = pd.concat([df, df_unique[df_unique["id"].isin(id_list_df)]])
-        s = df.sum(axis=0).squeeze()
+        df5 = df_nonunique[df_nonunique["id"].str.contains('|'.join(id_list_df_var))]
+        df5 = pd.concat([df5, df_unique[df_unique["id"].isin(id_list_df)]])
+        s = df5.sum(axis=0).squeeze()
         s.loc["descricao"] = "Despesa Fixa"
         s.loc["id"] = "DR_DF"
         s = pd.DataFrame(data=[s.values], columns=s.index)
-        self.dr = pd.concat([self.dr, df, s])
+        self.dr = pd.concat([self.dr, df5, s])
 
         # Lucro Bruto
         df_values = self.dr[self.dr["id"].isin(["DR_MC", "DR_DF"])].iloc[:, 2:14]
-        df = df_values.iloc[0, :] - df_values.iloc[1, :]
-        df.loc["id"] = "DR_LB"
-        df.loc["descricao"] = "Lucro Bruto Operacional"
-        df = pd.DataFrame(data=[df.values], columns=df.index)
-        self.dr = pd.concat([self.dr, df])
+        df6 = df_values.iloc[0, :] - df_values.iloc[1, :]
+        df6.loc["id"] = "DR_LB"
+        df6.loc["descricao"] = "Lucro Bruto Operacional"
+        df6 = pd.DataFrame(data=[df6.values], columns=df6.index)
+        self.dr = pd.concat([self.dr, df6])
 
         # Receitas Financeiras
         id_list_rf = ['JMRC', 'RAF', 'ORF']
-        df = df_unique[df_unique["id"].isin(id_list_rf)]
-        s = df.sum(axis=0).squeeze()
+        df7 = df_unique[df_unique["id"].isin(id_list_rf)]
+        s = df7.sum(axis=0).squeeze()
         s.loc["descricao"] = "Receitas Financeiras"
         s.loc["id"] = "DR_RF"
         s = pd.DataFrame(data=[s.values], columns=s.index)
-        self.dr = pd.concat([self.dr, df, s])
+        self.dr = pd.concat([self.dr, df7, s])
 
         # Despesas Financeiras
         id_list_dff = ['DF_01', 'GG_21', 'GG_22', 'DF_02']
-        df = df_unique[df_unique["id"].isin(id_list_dff)]
-        s = df.sum(axis=0).squeeze()
+        df8 = df_unique[df_unique["id"].isin(id_list_dff)]
+        s = df8.sum(axis=0).squeeze()
         s.loc["descricao"] = "Despesas Financeiras"
         s.loc["id"] = "DR_DFIN"
         s = pd.DataFrame(data=[s.values], columns=s.index)
-        self.dr = pd.concat([self.dr, df, s])
+        self.dr = pd.concat([self.dr, df8, s])
 
         # Compute Resultado da Tesouraria
         id_list_ct = ['VI', 'AS', 'REB', 'RET', 'RES', 'DL', 'OEO']
         df_ct = df_unique[df_unique["id"].isin(id_list_ct)]
         s_ct = df_ct.sum(axis=0).squeeze()
         s_ct.loc["descricao"] = "Resultado de Caixa Tesouraria"
-        s_ct.loc["id"] = "DR_DFIN"
+        s_ct.loc["id"] = "DR_RCT"
         s_ct = pd.DataFrame(data=[s_ct.values], columns=s_ct.index)
 
         # Lucro Bruto antes do IRPJ e CSLL
         df_values = self.dr[self.dr["id"].isin(["DR_LB", "DR_RF"])].iloc[:, 2:14]
-        df = df_values.iloc[0, :] - df_values.iloc[1, :] + s_ct.iloc[:, 2:14]
-        df = df.squeeze()
-        df["id"] = "DR_LBIC"
-        df["descricao"] = "Lucro Bruto antes do IRPJ e CSLL"
-        df = pd.DataFrame(data=[df.values], columns=df.index)
-        self.dr = pd.concat([self.dr, df])
+        df9 = df_values.iloc[0, :] - df_values.iloc[1, :] + s_ct.iloc[:, 2:14]
+        df9 = df9.squeeze()
+        df9["id"] = "DR_LBIC"
+        df9["descricao"] = "Lucro Bruto antes do IRPJ e CSLL"
+        df9 = pd.DataFrame(data=[df9.values], columns=df9.index)
+        self.dr = pd.concat([self.dr, df9])
 
         # adionar IRPJ/CSS
-        # TODO: Compute simples and add to the dr table
 
         # Lucro Liquido Final
-        df = self.dr[self.dr["id"] == "DR_LBIC"].loc[2:14] - self.irpj
-        df["id"] = "DR_LLF"
-        df["descricao"] = "Lucro Bruto antes do IRPJ e CSLL"
-        self.dr = pd.concat([self.dr, df])
+        df10 = self.dr[self.dr["id"] == "DR_LBIC"].iloc[:, 2:14] # - self.irpj  # TODO: Compute simples () and add to the dr table
+        df10["id"] = "DR_LLF"
+        df10["descricao"] = "Lucro Liquido Final"
+        self.dr = pd.concat([self.dr, df10])
 
         # Resultado de Caixa de tesouraria
         id_list_ctup = ['PCNPJ', 'GG_19', 'GG_20', 'MK_01', 'PL', 'DL']
@@ -173,11 +175,36 @@ class Economista:
         # Ponto de equilibrio economico
         df_dr = self.dr[self.dr["id"].str.contains("DR_")]
         s_mc = df_dr[df_dr["id"] == "DR_MC"].iloc[:, 2:14] / df_dr[df_dr["id"] == "DR_RL"].iloc[:, 2:14]
-        s_pef = df_dr[df_dr["id"] == "DR_DF"].iloc[:, 2:14] / s_mc
         s_lucro_desejado = self.data[self.data["id"] == "LD"].iloc[:, 2:14]
-        s_pee = pd.concat([df_dr[df_dr["id"] == "DR_DF"].iloc[:, 2:14], s_lucro_desejado]).sum(axis=0)/s_mc
 
-        # TODO: termianr ponto de equilibro de caixa
+        # compute pontos de equilibrio
+        s_pef = df_dr[df_dr["id"] == "DR_DF"].iloc[:, 2:14] / s_mc
+
+        s_pee = pd.concat([df_dr[df_dr["id"] == "DR_DF"].iloc[:, 2:14], s_lucro_desejado]).sum(axis=0) / s_mc
+        s_pec = pd.concat([df_dr[df_dr["id"] == "DR_DF"].iloc[:, 2:14],
+                           df_dr[df_dr["id"] == "DR_RF"].iloc[:, 2:14] * (-1),
+                           df_dr[df_dr["id"] == "DR_DFIN"].iloc[:, 2:14],
+                           df_ctup.iloc[:, 2:14], df_ctupin.iloc[:, 2:14],
+                           df_ct.iloc[:, 2:14] * (-1)
+                           ]).sum(axis=0) / s_mc
+
+        # name pontos de equilibrio
+        s_pef["id"] = "DR_PEF"
+        s_pee["id"] = "DR_PEE"
+        s_pec["id"] = "DR_PEC"
+        s_pef["descricao"] = "Ponto de Equilibrio Financeiro"
+        s_pee["descricao"] = "Ponto de Equilibrio Economico"
+        s_pec["descricao"] = "Ponto de Equilibrio de Caixa"
+
+        # add pontos de equilibrio
+        self.dr = pd.concat([self.dr, s_pef, s_pee, s_pec]).reset_index(drop=True)
+
+        pass
+
+    def compute_dr_unique(self):
+        df = pd.DataFrame(self.dr["id"].value_counts() > 1)
+        self.repeted_ids = df[df["id"] == True].index
+        self.dr_unique = self.dr.drop_duplicates("id")
         pass
 
     def compute_indices(self):
@@ -229,9 +256,8 @@ class Economista:
         pass
 
     def debito_longo(self):
-        # TODO: 1. modify the velue of debitos futuros. Parece um input ate agora
         id_list = ["DFLP"]
-        df_dl = self.data[self.data["id"].isin(id_list)]  # MODIFICAR
+        df_dl = self.data[self.data["id"].isin(id_list)]
         df_dl["pas/ati"] = "passivo"
         df_dl["tipo"] = "debitos futuros de longo prazo"
 
@@ -246,7 +272,7 @@ class Economista:
         capital_de_terceiros = self.balanco[(self.balanco["pas/ati"] == "passivo")
                                             & (self.balanco["tipo"] == "capital de terceiros")].sum(axis=0)[2:14]
         debito_longo_prazo = self.balanco[self.balanco["tipo"] == "debitos futuros de longo prazo"].sum(axis=0)[2:14]
-        resultado_exercicio = ativos.replace(ativos.values, 234.29)  # TODO: modificar esse valor para valor real da DR
+        resultado_exercicio = self.dr[self.dr["id"] =="DR_LLF"].iloc[:, 2:14].squeeze()  # TODO: modificar esse valor para valor real da DR
         capital_social_subscrito = self.data[self.data["id"].isin(id_list)].iloc[:, 2:14].squeeze()
         capital_proprio = ativos - (capital_de_terceiros
                                     + debito_longo_prazo
@@ -341,6 +367,8 @@ class Economista:
         df_final = pd.concat([df_cg, df_dt, df_em, df_dap, df_clig, df_gi, df_dv, df_ea])
         df_final["pas/ati"] = "outros"
         df_final["tipo"] = "outros"
-        self.balanco = pd.concat([self.balanco, df_final])
+        self.balanco = pd.concat([self.balanco, df_final]).reset_index(drop=True)
 
         pass
+
+
